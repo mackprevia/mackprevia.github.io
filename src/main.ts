2b11c30a87734bf568e16d5828006a949227898a
Lucas <61513630+lsglucas@users.noreply.github.com>
2021-11-27 00:38:51 -0300

    Fix to build and dependencies (#3)
    * Finshed starter version
    
    * Fixed cache failure changes and build erros
    
    Co-authored-by: danielkauffmann <danielvenna2@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { store, key } from "./stores";
 import router from "@/routes";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 
 const app = createApp(App);
-
-library.add(faUserSecret)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(store, key);
 
