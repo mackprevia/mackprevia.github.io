5092a73a74a79fd30895d7814c93fca44972e953
lsglucas <lsglucas@protonmail.com>
2021-11-28 16:35:58 -0300

    Added locale change option

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 import {vueI18n} from "@intlify/vite-plugin-vue-i18n";
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        cssCodeSplit: true,
+        sourcemap: "inline"
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
